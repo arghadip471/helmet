849a4a120087df2b4df6a93603b7e13764024e6a
arghadip <chowdhuryarghadip471@gmail.com>
2024-12-11 20:09:13 +0530

    update

--- recieve_data_rfid.php
+++ recieve_data_rfid.php
@@ -33,21 +33,21 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $row = $result->fetch_assoc();
         $area = $row['area'];
 
-        if ($area === "area_1") {
+        if ($area === "area_default") {
             // Update entry_time
             $sql_update = "UPDATE employeedata 
-                           SET entry_time = '$current_time', area = 'area_default' 
-                           WHERE employee_id = '$employee_id' AND area = 'area_1'";
+                           SET entry_time = '$current_time', area = 'area_1', exit_time='0' 
+                           WHERE employee_id = '$employee_id' AND area = 'area_default'";
             if ($conn->query($sql_update) === TRUE) {
                 echo "Entry time updated successfully.";
             } else {
                 echo "Error updating entry time: " . $conn->error;
             }
-        } elseif ($area === "area_default") {
+        } elseif ($area === "area_1") {
             // Update exit_time
             $sql_update = "UPDATE employeedata 
-                           SET exit_time = '$current_time', area = 'area_1'  
-                           WHERE employee_id = '$employee_id' AND area = 'area_default'";
+                           SET exit_time = '$current_time', area = 'area_default'  
+                           WHERE employee_id = '$employee_id' AND area = 'area_1'";
             if ($conn->query($sql_update) === TRUE) {
                 echo "Exit time updated successfully.";
             } else {
